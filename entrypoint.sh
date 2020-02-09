ca1f8e0a6b7eafaaeec5630005dd09b4a7fd6c2e
Zhiyuan Zheng <me@xmflsct.com>
2020-02-09 21:56:18 +0100

    Add Contentful trigger condition

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,9 @@ set -e
 
 yarn
 CONTENTFUL_SPACEID=$5 CONTENTFUL_ACCESSTOKEN=$6 gatsby build
-yarn run gh-pages -d public -b $1 -u ''"$3"' ''<'"$4"'>' -r 'https://'"$2"'@github.com/'"$GITHUB_REPOSITORY"'.git' -m 'Build of '"$GITHUB_SHA"''
+if [$7 -eq 1]
+  then
+    yarn run gh-pages -d public -b $1 -u ''"$3"' ''<'"$4"'>' -r 'https://'"$2"'@github.com/'"$GITHUB_REPOSITORY"'.git' -m 'Build of '"$GITHUB_SHA"''
+  else
+    yarn run gh-pages -d public -b $1 -u ''"$3"' ''<'"$4"'>' -r 'https://'"$2"'@github.com/'"$GITHUB_REPOSITORY"'.git' -m 'Build by publishing on Contentful'
+fi
